d171664ae5b27f560529c5bd64e7247d38de6e07
wangxianfa <1270386187@qq.com>
2017-09-03 22:19:42 +0800

    token生成、登录页面访问

--- router/login.js
+++ router/login.js
@@ -1,4 +1,6 @@
 const loginModel = require('../models/loginSchema')
+const jwt = require('jsonwebtoken')
+const token = require('../config/configurable/token')
 
 exports.doLogin = (req, res) => {
 
@@ -12,8 +14,37 @@ exports.doLogin = (req, res) => {
 
   req.on('end', () => {
 
-    console.log(JSON.parse(reqBody))
+    const { username, password } = JSON.parse(reqBody)
 
-    res.send('1')
+    loginModel.vertifyCont(username, password, (err, result) => {
+
+      if (err) {
+        res.send({'code': 'E01', 'message': '登录验证失败', 'content': ''})
+        return;
+      }
+
+      if (result.length === 0) {
+        res.send({'code': 'S03', 'message': '用户名不存在或密码错误', 'content': ''})
+        return;
+      }
+
+      // 生成token
+      jwt.sign({username: username, privilgeLevel: 1}, 'wangxianfa vertify', token.options, function(err, token) {
+        console.log(token)
+
+        jwt.verify(token, 'wangxianfa vertify', function(err, decoded) {
+          if (err) {
+            console.log(err)
+          } else {
+            console.log(decoded)
+          }
+        })
+
+        res.setHeader("token", token);
+        res.send({'code': 'S01', 'message': '登录验证成功', 'content': token})
+      });
+
+    })
+
   })
 }
